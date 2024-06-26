624516f8d7ce34134c036e2a935cf5daba910e08
Shalabh Agarwal <agarwalshalabh98@gmail.com>
2024-06-26 16:18:19 +0530

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// Astro configuration
+export default defineConfig({
+  // Other configuration options...
+  vite: {
+    ssr: {
+      noExternal: ['sharp'],
+    },
+  },
+});
